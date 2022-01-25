32620fb7dfce700e38f9e47f7e2c913f19b68518
D1nCoRe <overkolskaya@gmail.com>
2022-01-25 15:15:54 +0300

    test rediscripted

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
-# pytest -v --tb=line --language=en test_main_page.py для запуска
-# 4.1 - 6 - 10-12p add
+from .pages.main_page import MainPage
 
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
@@ -7,5 +6,6 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
